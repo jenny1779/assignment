6d8faa7657a1c87f22a530ba23fb17f10850fe00
Peter Zheng <zhijiazheng@microsoft.com>
2024-04-03 20:53:34 +0800

    test

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var sum int
-	for i := 0; i < 5; i++ {
-		sum += i
+	var arr []int = []int{1, 2, 3, 6, 7}
+	for i, val := range arr {
+		fmt.Println(i, val)
 	}
-	fmt.Printf("sum is %d\n", sum)
 }
